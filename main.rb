68d5a3da7865541b0d7f16d7bc4d45be92eb8c0c
Marc Seeger <mail@marc-seeger.de>
2012-04-11 01:07:51 +0200

    initial commit

---
+++ main.rb
@@ -0,0 +1,58 @@
+require 'rubygems'
+require 'airvideo' #gem install airvideo-ng --> https://github.com/kalw/AirVideo
+require 'sinatra'
+require "base64"
+
+configure do
+  $airvideo = AirVideo::Client.new('something.dyndns.org',45631,'YOUR_PASSWORD')
+  $airvideo.max_width = 640
+  $airvideo.max_height = 480
+end
+
+get '/' do
+	$airvideo.cd('/')
+	items = $airvideo.ls
+  @folders = items.select{|item| item.is_a? AirVideo::Client::FolderObject}
+  @videos = items.select{|item| item.is_a? AirVideo::Client::VideoObject}
+  erb :index
+end
+
+get '/folder/:folder_location' do
+  path = Base64.decode64(params[:folder_location])
+  $airvideo.cd(path)
+  items = $airvideo.ls
+  @folders = items.select{|item| item.is_a? AirVideo::Client::FolderObject}
+  @videos = items.select{|item| item.is_a? AirVideo::Client::VideoObject}
+  erb :index
+end
+
+
+get '/play/:video_location/:playback_mode/:bitrate' do
+  playback_mode = params[:playback_mode]
+  bitrate = params[:bitrate]
+  video_location  = Base64.decode64(params[:video_location])
+  video_dir = video_location.split('/')[0..-2].join('/') + "/"
+  video_name  = video_location.split('/').last
+  $airvideo.cd(video_dir)
+  video_file = $airvideo.ls.select{|item| item.name == video_name}.first
+  case playback_mode
+  when 'native'
+    @video_live_url  = video_file.url
+  when 'convert'
+    already_retried = false
+    begin
+      @video_live_url  = video_file.live_url + "?q=#{bitrate}"
+    rescue
+      unless already_retried
+        already_retried = true
+        retry
+      end
+      return "Airvideo can't convert this file, sorry"
+    end
+  else
+    raise 'unknown playback mode: #{playback_mode.inspect}'
+  end
+  
+  erb :play
+
+end
